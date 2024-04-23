96040496c03d7bf5637b71a5e93aaf80d082f679
Vitor Melech Venante <vitor.venante@hotmail.com>
2024-04-22 22:10:28 -0300

    conclusão CRUD 2

--- grupos/urls.py
+++ grupos/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from .views import home, salvar, editar, atualizar, deletar
 
 urlpatterns = [
+    #CadastroPoliciais
     path('', home),
     path('salvar/',             salvar,     name="salvar"),
     path('editar/<int:id>',     editar,     name="editar"),
